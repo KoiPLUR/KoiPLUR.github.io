53b996dca7db0ac01160c8252aa15ac620ed14ab
KoiPLUR <koi.plur@gmail.com>
2021-11-05 22:57:05 +0800

    create mint function

---
+++ precache-manifest.29326cee147ac4a15a30e54864571730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4132c51968e7a18dc4f9faac19fa95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d62add13a2abda16f564",
+    "url": "/static/css/main.e1b91f5f.chunk.css"
+  },
+  {
+    "revision": "e1626df8393c7889fc6e",
+    "url": "/static/js/2.ca903867.chunk.js"
+  },
+  {
+    "revision": "d62add13a2abda16f564",
+    "url": "/static/js/main.8420c009.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  },
+  {
+    "revision": "52daf854e69adcd8b6015f1fe01d28d3",
+    "url": "/static/media/logo.52daf854.png"
+  }
+]);
